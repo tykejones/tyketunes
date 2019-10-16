8c8c1fb896a9468fe325ef3e90dd31886ba09443
Tyke Jones <tykejones@gmail.com>
2019-10-16 13:46:31 -0700

    Create a React Application Static Components, Pass Down Search Result and Render Result List

---
+++ src/Components/App/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, {name: 'name2', artist: 'artist2', album: 'album2', id: 2}, {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
+    }
+    
+    }
+    
+    
+    render() {
+        return (
+        <div>
+            <h1>tyke<span className="highlight">tunes</span></h1>
+            <div className="App">
+                <SearchBar />
+                <div className="App-playlist">
+                <SearchResults searchResults={this.state.searchResults} />
+                <Playlist />
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default App;
